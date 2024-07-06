ff3dbdf1ee7e4d8b91956e1c3d89bf6352e6c0f7
tkyoo <dbxorms4712@gmail.com>
2024-07-06 11:31:19 +0900

    최초커밋

---
+++ src/P1000_A더하기B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class P1000_A더하기B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A, B;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        System.out.println(A + B);
+    }
+}
