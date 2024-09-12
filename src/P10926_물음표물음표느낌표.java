6921cff4a7149f4603e60c52f0e4ee861df8fb2c
YooTaeKeun <38456529+YooGree@users.noreply.github.com>
2024-09-12 12:28:43 +0900

    Create P10926_물음표물음표느낌표.java

---
+++ src/P10926_물음표물음표느낌표.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P10926_물음표물음표느낌표 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+
+        System.out.println(input + "??!");
+    }
+}
