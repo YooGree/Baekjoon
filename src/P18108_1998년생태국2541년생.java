99519976aa19e1f799ed2d489e409fed741f0672
YooTaeKeun <38456529+YooGree@users.noreply.github.com>
2024-09-13 13:03:01 +0900

    Create P18108_1998년생태국2541년생.java

---
+++ src/P18108_1998년생태국2541년생.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P18108_1998년생태국2541년생 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int y = sc.nextInt();
+
+        System.out.println(y - 543);
+    }
+}
