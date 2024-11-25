4a1e7a74edd440079760a28ebe8e4a405cfd8ffc
YooGree <@tk951753@>
2024-11-25 15:55:22 +0900

    P27323, P1085 추가

---
+++ src/P27323_직사각형.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class P27323_직사각형 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A * B);
+    }
+}
