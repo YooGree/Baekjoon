ccd8da333f75b3ac3b21475552bde9ec5b68e925
YooTaeKeun <38456529+YooGree@users.noreply.github.com>
2024-12-02 12:34:58 +0900

    Create P24262_알고리즘의수행시간1.java

---
+++ src/P24262_알고리즘의수행시간1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class P24262_알고리즘의수행시간1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextInt();
+
+        int count = 1; // 코드1 수행횟수
+        int degree = 0; // 최고차항 갯수
+
+        System.out.println(count);
+        System.out.println(degree);
+    }
+}
