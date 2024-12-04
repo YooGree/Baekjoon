bec0160c833bd31c30cd93ec81cb9887562ea479
YooTaeKeun <38456529+YooGree@users.noreply.github.com>
2024-12-04 12:38:28 +0900

    Create P24264_알고리즘의수행시간3.java

---
+++ src/P24264_알고리즘의수행시간3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class P24264_알고리즘의수행시간3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextInt();
+
+        System.out.println(n * n);
+        System.out.println(2);
+    }
+}
