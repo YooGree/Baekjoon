2ae3ffa1c9d63c522e8d3b75e0e3a3fc1d2b6a34
YooGree <@tk951753@>
2025-06-10 22:56:22 +0900

    P10699 추가

---
+++ src/P10699_오늘날짜.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class P10699_오늘날짜 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+}
