5614de8165cdaf282b5aa69657d64b8cae9b8d3b
유태근 <dbxorms4712@naver.com>
2025-08-12 19:23:47 +0900

    P4470 추가

---
+++ src/P4470_줄번호.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P4470_줄번호 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.nextLine();
+
+        String[] arr = new String[N];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextLine();
+            System.out.println((i + 1) + ". " + arr[i]);
+        }
+        sc.close();
+    }
+}
