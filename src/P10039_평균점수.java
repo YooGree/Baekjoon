04868b22e755a6f1c560d30ac633a11f5521145b
YooGree <@tk951753@>
2025-08-18 22:05:40 +0900

    P10039 추가

---
+++ src/P10039_평균점수.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P10039_평균점수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num < 40) num = 40;
+            sum += num;
+        }
+
+        System.out.println(sum / 5);
+    }
+}
