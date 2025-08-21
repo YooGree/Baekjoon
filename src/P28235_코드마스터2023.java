0d46d8b35af8543c7b4a7ccec82d9aac20de2ff1
YooGree <@tk951753@>
2025-08-21 23:42:15 +0900

    P28235 추가

---
+++ src/P28235_코드마스터2023.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class P28235_코드마스터2023 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+
+        if(a.equals("SONGDO")) {
+            System.out.println("HIGHSCHOOL");
+        }else if(a.equals("CODE")) {
+            System.out.println("MASTER");
+        }else if(a.equals("2023")) {
+            System.out.println("0611");
+        }else if(a.equals("ALGORITHM")) {
+            System.out.println("CONTEST");
+        }
+        sc.close();
+    }
+}
