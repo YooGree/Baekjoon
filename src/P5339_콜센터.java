5306d89e679c179ef8390080c5e806e0a0c28eab
tkyoo <dbxorms4712@naver.com>
2025-08-28 13:06:14 +0900

    P5339 추가

---
+++ src/P5339_콜센터.java
@@ -0,0 +1,14 @@
+public class P5339_콜센터 {
+    public static void main(String[] args) {
+        System.out.println("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
