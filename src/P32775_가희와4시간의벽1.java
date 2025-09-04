4d60958fd0e35efd21374de82fedc97e5bbd4a1d
YooGree <@tk951753@>
2025-09-04 22:43:30 +0900

    P32775 추가

---
+++ src/P32775_가희와4시간의벽1.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P32775_가희와4시간의벽1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int S = Integer.parseInt(br.readLine());   // 고속철도
+        int F = Integer.parseInt(br.readLine());   // 항공편
+
+        // 소요 시간 적게 걸리는 교통 수단을 이용
+        if(S <= F)
+        {
+            System.out.println("high speed rail");
+        }
+        else System.out.println("flight");
+
+    }
+}
