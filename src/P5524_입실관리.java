b0f6e6ba942ee1f4749d3426bf86371d5d1b2a32
tkyoo <dbxorms4712@naver.com>
2025-09-17 18:05:44 +0900

    P5524 추가

---
+++ src/P5524_입실관리.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P5524_입실관리 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (n-->0) {
+            String cur = br.readLine();
+            sb.append(cur.toLowerCase()).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
