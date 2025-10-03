84574dabfcd7b4bc428df52c865d55ae59ea6051
YooGree <@tk951753@>
2025-10-03 21:31:20 +0900

    P10189 추가

---
+++ src/P10189_HOOK.java
@@ -0,0 +1,8 @@
+public class P10189_HOOK {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
