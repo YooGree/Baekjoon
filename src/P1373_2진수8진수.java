de51db8937b108c44e47009d73d080adb7def1f3
YooGree <@tk951753@>
2025-10-08 22:39:04 +0900

    P1373 추가

---
+++ src/P1373_2진수8진수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class P1373_2진수8진수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        BigInteger N = new BigInteger(str, 2);
+
+        String result = N.toString(8);
+
+        System.out.println(result);
+    }
+}
