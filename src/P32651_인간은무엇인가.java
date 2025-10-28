a851c7a7f551d97318e97eeb43dd19c95e6eb484
YooGree <@tk951753@>
2025-10-28 22:23:30 +0900

    P32651 추가

---
+++ src/P32651_인간은무엇인가.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P32651_인간은무엇인가 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if (N < 100000 && N % 2024 == 0){
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
