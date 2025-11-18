27461f5d39aea399cd41dc8f51424077cbe5106b
tkyoo <dbxorms4712@naver.com>
2025-11-18 12:56:45 +0900

    P34721 추가

---
+++ src/P34721_역사를걸으면동국이보이고.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P34721_역사를걸으면동국이보이고 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            System.out.println("I love DGU");
+        }
+    }
+}
