d41ecf4126247ee9347044c0f91638df9af256fe
tkyoo <dbxorms4712@gmail.com>
2025-12-04 23:54:59 +0900

    P2721 추가

---
+++ src/P2721_삼각수의합.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2721_삼각수의합 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < T; i++) {
+            int sum = 0;
+            int n = Integer.parseInt(br.readLine());
+            for(int j = 1; j <= n; j++) {
+                int A = 0;
+
+                for(int k = 1; k <= (j + 1); k++) {
+                    A += k;
+                }
+
+                sum += (j * A);
+            }
+            System.out.println(sum);
+        }
+
+    }
+}
