bc25a20499150c12c0c54e95bf7f77c14dac898c
YooGree <@tk951753@>
2026-01-18 23:16:58 +0900

    P3448 추가

---
+++ src/P3448_문자인식.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P3448_문자인식 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int r = 0;
+            int a = 0;
+
+            while (true) {
+                String s = br.readLine();
+                if (s.equals("")) { break; }
+
+                r += s.replaceAll("#", "").length(); // 인식한 문자의 수
+                a += s.length(); // 전체 문자의 수
+            }
+            double recognitionRate = (double) r / a * 100;
+            recognitionRate = Math.round(recognitionRate * 10) / 10.0;
+
+            if (recognitionRate == (int) recognitionRate) {
+                System.out.printf("Efficiency ratio is %d%%.\n", (int) recognitionRate);
+            } else {
+                System.out.printf("Efficiency ratio is %.1f%%.\n", recognitionRate);
+            }
+        }
+    }
+}
