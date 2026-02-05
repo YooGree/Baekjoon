76f815a2fff8fd22254eef8fe90cd2767dd62bf9
tkyoo <dbxorms4712@gmail.com>
2026-02-05 20:13:53 +0900

    P14910 추가

---
+++ src/P14910_오르막.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class P14910_오르막 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        String ret = "Good";
+        String[] str = sc.nextLine().split(" ");
+
+        for (int i = 0; i < str.length - 1; i++) {
+            int a = Integer.parseInt(str[i]);
+            int b = Integer.parseInt(str[i + 1]);
+            if (a > b) {
+                ret = "Bad";
+                break;
+            }
+        }
+
+        System.out.println(ret);
+    }
+}
