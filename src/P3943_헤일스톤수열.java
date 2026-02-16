7a561fc1358ae5f13594def878c34acca605673b
유태근 <dbxorms4712@naver.com>
2026-02-16 23:23:15 +0900

    P3943 추가

---
+++ src/P3943_헤일스톤수열.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P3943_헤일스톤수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(bf.readLine());
+        for(int i=0; i<T; i++) {
+            int n = Integer.parseInt(bf.readLine());
+            int max = n;
+            while(n!=1) {
+                n = (n%2 == 0) ? n/2 : (n*3)+1;
+                max = Math.max(max, n);
+            }
+            System.out.println(max);
+        }
+        bf.close();
+    }
+}
