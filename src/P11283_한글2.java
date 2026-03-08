0cbf120f58aa705d0604a4f674700b1c2165e458
YooGree <@tk951753@>
2026-03-09 00:03:04 +0900

    P11283 추가

---
+++ src/P11283_한글2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class P11283_한글2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        char hangul = sc.nextLine().charAt(0);
+        sc.close();
+
+        System.out.println((int) hangul - 44031); // '가'라고 찍으면 44032가 나옴. '가'가 1이되려면 44031 빼주기
+
+    }
+}
